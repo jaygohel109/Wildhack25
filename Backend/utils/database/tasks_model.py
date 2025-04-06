e5778609eb9970e33a62a965d6033b26933af7e2
root <root@LAPTOP-CLB3JM5H.localdomain>
2025-04-05 22:51:01 -0500

    feat: create tasks api

---
+++ Backend/utils/database/tasks_model.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import Optional, List
+from enum import IntEnum
+
+class Priority(str, Enum):
+    urgent = "urgent"
+    intermediate = "intermediate"
+    normal = "normal"
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+class Category(IntEnum):
+    TECH_HELP = 1
+    TRANSPORTATION = 2
+    HOME_REPAIRS = 3
+    COMPANIONSHIP = 4
+    GROCERY_SUPPORT = 5
+    OTHER = 6
+
+class TasksRequest(BaseModel):
+    issue: str
+    priority: Priority
+    preferable_gender: Gender
+    description: str
+    category: Category
+    status: str = "open"
+    created_by: str  # required to embed into correct user
+    
+class AssignTasks(BaseModel):
+    task_id: str
+    volunteer_id: str
